6baa5195c41408a766f31ccbc9d2f9cfec3622a0
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-11 14:41:54 +0300

    Change navbar title

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,19 +4,19 @@ import ThemeContext from './ThemeContext';
 import styles from './styles/navbar.module.css'
 
 function Navbar() {
-  const {toggleDarkMode, isDarkMode} = useContext(ThemeContext);
+  const { toggleDarkMode, isDarkMode } = useContext(ThemeContext);
   return (
     <nav className={styles.navbar}>
-        <h1>Blog</h1>
-        <div className={styles.links}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/newblog" className={styles.newBlog}>New Blog</NavLink>
-            <label className={styles.switch}>
-              <input type="checkbox" checked={isDarkMode} onChange={toggleDarkMode}/>
-              <span className={styles.slider}></span>
-            </label>
-        </div>
-       
+      <h1>iWonder</h1>
+      <div className={styles.links}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/newblog" className={styles.newBlog}>New Blog</NavLink>
+        <label className={styles.switch}>
+          <input type="checkbox" checked={isDarkMode} onChange={toggleDarkMode} />
+          <span className={styles.slider}></span>
+        </label>
+      </div>
+
     </nav>
   )
 }
