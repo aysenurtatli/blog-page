2bf16163b18aad2f75b8ff33279bf8b4fc466ee3
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-11 14:40:33 +0300

    spinner bg color change

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useContext } from 'react';
+import ThemeContext from './ThemeContext'
+
 function Spinner() {
+  const { isDarkMode } = useContext(ThemeContext);
   return (
-    <div className="loading-screen">
+    <div className="loading-screen" style={isDarkMode ? { backgroundColor: "#1f1f1f" } : { backgroundColor: "#faf9f9" }}>
       <div className="spinner center">
-          <div className="loader"></div>
+        <div className="loader"></div>
       </div>
     </div>
   )
