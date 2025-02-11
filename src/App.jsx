256e328fe5111da01c0b168ac43fa42bf03fc9b0
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-11 14:41:35 +0300

    remove unnecesarry imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import NewBlog from './NewBlog';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
 import Footer from './Footer';
-import ThemeContext from './ThemeContext'
-import { useContext } from 'react';
 import { ThemeProvider } from './ThemeContext';
 function App() {
   return (
     <>
-    <ThemeProvider>
-    <Router>
-          <Navbar/>
+      <ThemeProvider>
+        <Router>
+          <Navbar />
           <div className='content'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/newblog' element={<NewBlog/>}/>
-            <Route path='/blogs/:id' element={<BlogDetails/>}/>
-            <Route path='*' element={<NotFound/>}></Route>
-          </Routes>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/newblog' element={<NewBlog />} />
+              <Route path='/blogs/:id' element={<BlogDetails />} />
+              <Route path='*' element={<NotFound />}></Route>
+            </Routes>
           </div>
-          <Footer/>
-      </Router>
+          <Footer />
+        </Router>
       </ThemeProvider>
     </>
   )
